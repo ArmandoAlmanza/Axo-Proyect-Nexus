962a6bcec95eb6f8d11272adb8b369a8c4629762
Armando Almanza <>
2024-01-14 14:03:52 -0600

    add base struct for go API

--- server/proyects/main.go
+++ server/proyects/main.go
@@ -1,7 +1,20 @@
-package main;
+package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
-	fmt.Println("Hello World")
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	fmt.Println("\033[45;30mListening on port 8000.\033[0m")
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":8000")
 }
