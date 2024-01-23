4cbdbb1d31196e6ce9702de36dc0570eeaec0dde
Armando Almanza <armando.s.almanza@gmail.com>
2024-01-23 09:41:38 -0600

    add hover cursor

--- client/src/components/projects/ProjectNav.tsx
+++ client/src/components/projects/ProjectNav.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const ProjectNav = ({ title, img }: Props) => {
     return (
-        <article className="flex w-50 items-center p-2 border rounded-xl transition-colors duration-300 hover:bg-white hover:border-black shadow-md">
+        <article className="flex w-50 items-center p-2 border rounded-xl transition-colors duration-300 hover:bg-white hover:border-black shadow-md cursor-pointer">
             <img className="h-20 rounded-full" src={img === "" ? "/axolotl.png" : img} alt="icon" />
             <p className="text-lg font-semibold">{title}</p>
         </article>
