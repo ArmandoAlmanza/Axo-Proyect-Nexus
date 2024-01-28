cfdb29d431ab84e6c37cda022176aee1dd215785
Armando Almanza <>
2024-01-28 14:01:56 -0600

    i guees i need to start again

---
+++ server/users/src/main/java/com/axoserver/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.axoserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	// Endpoints
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(
+						authRequest -> authRequest.requestMatchers("/auth/**").permitAll()
+								.anyRequest().authenticated())
+				.formLogin(withDefaults())
+				.build();
+	}
+}
---
+++ server/users/src/main/java/com/axoserver/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.axoserver.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("api/v1/demo")
+@RequiredArgsConstructor
+public class DemoController {
+
+	@PostMapping("demo")
+	public String welcome() {
+
+		return "Welcome to my api";
+	}
+
+}
