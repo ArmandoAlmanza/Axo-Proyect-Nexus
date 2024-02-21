0a0189a584fd96a241b0ed38416a8d71bc0529a1
Armando Almanza <armando.s.almanza@gmail.com>
2024-02-21 13:54:02 -0600

    update color pallete

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,33 @@ export default {
                 body: "#F9F5F6",
                 purple: "rgba(202, 3, 252, 0.38)",
                 "purple-light": "#B2A4FF",
+                lily: {
+                    100: "rgb(232, 210, 255)",
+                    200: "rgb(225, 196, 255)",
+                    300: "rgb(209, 180, 255)",
+                },
+                bluebell: {
+                    100: "rgb(173, 216, 230)",
+                    200: "rgb(112, 159, 192)",
+                    300: "rgb(51, 102, 153)",
+                },
+                noir: {
+                    100: "rgb(51, 51, 51)",
+                    200: "rgb(33, 33, 33)",
+                    300: "rgb(25, 25, 25)",
+                },
+                leaf: {
+                    100: "#D1F2EB",
+                    200: "#C7ECEE",
+                },
+                dalia: {
+                    100: "#FFF9D9",
+                    200: "#F7E7B2",
+                },
+                garnet: {
+                    100: "#FDD6C5",
+                    200: "#F8B195",
+                },
             },
             fontFamily: {
                 primary: ["Montserrat", "sans-serif"],
