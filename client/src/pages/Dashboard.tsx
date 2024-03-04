dbbb35db0c5eb801f25699d5e518ec1afb3ca48a
Armando Almanza <armando.s.almanza@gmail.com>
2024-03-04 09:42:01 -0600

    style the footer

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import useScreenSize from "../hooks/useScreenSize";
 const Dashboard = () => {
+    const screenSize = useScreenSize();
     return (
-        <main className="w-full">
+        <main className="w-full grid">
             <Header />
-            
+            <Outlet />
+            {screenSize.width <= 390 ? <Footer /> : ""}
         </main>
     );
 };
