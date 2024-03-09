bdbea3342be080156a8bd310b5e34db41dc7da95
Armando Almanza <>
2024-03-09 08:28:01 -0600

    add devices sizes

---
+++ client/src/utils/sizes.ts
@@ -0,0 +1,16 @@
+export const devices = {
+    mobile: {
+        width: 360,
+    },
+    tablet: {
+        width: 800,
+    },
+    laptop: {
+        width: 1000,
+        height: 1023,
+    },
+    desktop: {
+        width: 1080,
+        height: 1024,
+    },
+};
