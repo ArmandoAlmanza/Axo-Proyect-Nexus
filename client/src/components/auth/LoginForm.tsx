e4b8e8809189eab15386c2d373a0f157d8ee05e4
Armando Almanza <>
2024-03-17 10:52:21 -0600

    delete eye icon for now

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { FaArrowRightToBracket } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import Button from "../generals/Button";
+import useValidation from "../../hooks/useValidation";
 
 interface formProps {
     state: string;
@@ -9,11 +10,33 @@ interface formProps {
 }
 
 const LoginForm = ({ setState }: formProps) => {
-    const [visible, setVisible] = useState(false);
+    const { userValidation, passValidation } = useValidation();
+
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [isValid, setIsValid] = useState({
+        user: false,
+        password: false,
+    });
+
     const navigate = useNavigate();
-    const toggleVisible = () => {
-        setVisible(!visible);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
+        event.target.name === "email"
+            ? setIsValid({
+                  ...isValid,
+                  user: userValidation(event.target.value, "email"),
+              })
+            : setIsValid({
+                  ...isValid,
+                  password: passValidation(event.target.value),
+              });
     };
+
     return (
         <>
             <form className="mb-4">
@@ -31,6 +54,8 @@ const LoginForm = ({ setState }: formProps) => {
                         required
                         id="email"
                         name="email"
+                        value={userInfo.email}
+                        onChange={handleInputChange}
                     />
                 </div>
                 <div className="flex flex-col mb-4">
@@ -41,24 +66,14 @@ const LoginForm = ({ setState }: formProps) => {
                         <span>Password </span>
                     </label>
                     <input
-                        className="oshadow appearance-none border-b-2 shadow-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-purple transition-[border,colors] duration-500"
-                        type={visible ? "text" : "password"}
+                        className="appearance-none border-b-2 shadow-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-purple transition-[border,colors] duration-500"
                         placeholder="*******"
                         required
                         id="password"
                         name="password"
+                        value={userInfo.password}
+                        onChange={handleInputChange}
                     />
-                    {!visible ? (
-                        <FaEye
-                            onClick={toggleVisible}
-                            className="absolute right-10 bottom-[130px] opacity-[65%]"
-                        />
-                    ) : (
-                        <FaEyeSlash
-                            onClick={toggleVisible}
-                            className="absolute right-10 bottom-[130px] opacity-[65%]"
-                        />
-                    )}
                 </div>
                 <button
                     className="cursor-pointer text-blue-600"
@@ -68,10 +83,15 @@ const LoginForm = ({ setState }: formProps) => {
                 </button>
                 <Button
                     type="button"
-                    onClick={() => navigate("/dashboard")}
-                    className="bg-black text-white px-2 py-1 mx-auto block w-36 transition-colors hover:bg-violet-400 hover:text-black duration-500"
+                    onClick={() =>
+                        isValid.user && isValid.password
+                            ? setTimeout(() => navigate("/dashboard"), 2000)
+                            : alert("user or password invalid")
+                    }
+                    className="bg-black text-white px-1 py-2 mx-auto w-3/4 transition-colors hover:bg-violet-400 hover:text-black duration-500 flex items-center content-center mt-2"
                 >
-                    <span>Log in</span>
+                    <span className="text-[20px]">Log In</span>
+                    <FaArrowRightToBracket className="text-[22px]" />
                 </Button>
             </form>
         </>
