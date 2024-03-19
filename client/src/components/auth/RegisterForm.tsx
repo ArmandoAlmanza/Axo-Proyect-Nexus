647ef83eab13600f3f8f0e2c80ec73cfe5a40bda
Armando Almanza <>
2024-03-19 17:58:19 -0600

    validate user register

--- client/src/components/auth/RegisterForm.tsx
+++ client/src/components/auth/RegisterForm.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { FaArrowRightToBracket } from "react-icons/fa6";
+
+import useValidation from "../../hooks/useValidation";
+import Button from "../generals/Button";
+import { useNavigate } from "react-router-dom";
 
 interface formProps {
     state: string;
@@ -7,11 +11,38 @@ interface formProps {
 }
 
 const RegisterForm = ({ setState }: formProps) => {
-    const [visible, setVisible] = useState(false);
-    const toggleVisible = () => {
-        setVisible(!visible);
+    const navigate = useNavigate();
+    const { userValidation, passValidation } = useValidation();
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        name: "",
+    });
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setUserInfo({ ...userInfo, [name]: value });
     };
 
+    const handleButtonClick = () => {
+        if (!userValidation(userInfo.email, "email")) {
+            alert("The user email is invalid");
+            return;
+        }
+        if (!userValidation(userInfo.name, "name")) {
+            alert(
+                "The user name is invalid, you can only use numbers and letters"
+            );
+            return;
+        }
+        if (!passValidation(userInfo.password)) {
+            alert("The password contains invalid characters");
+            return;
+        }
+
+        alert("All good, welcome");
+        setTimeout(() => navigate("/dashboard"), 2000);
+    };
     return (
         <div className="transition-opacity ease-in-out opacity-100">
             <form className="mb-4">
@@ -29,6 +60,8 @@ const RegisterForm = ({ setState }: formProps) => {
                         required
                         id="name"
                         name="name"
+                        value={userInfo.name}
+                        onChange={handleInputChange}
                     />
                 </div>
                 <div className="flex flex-col mb-4">
@@ -45,6 +78,8 @@ const RegisterForm = ({ setState }: formProps) => {
                         required
                         id="email"
                         name="email"
+                        value={userInfo.email}
+                        onChange={handleInputChange}
                     />
                 </div>
                 <div className="flex flex-col mb-4">
@@ -56,30 +91,24 @@ const RegisterForm = ({ setState }: formProps) => {
                     </label>
                     <input
                         className="appearance-none border-b-2 shadow-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-purple transition-[border,colors] duration-500"
-                        type={visible ? "text" : "password"}
+                        type={"password"}
                         placeholder="*******"
                         required
                         id="password"
                         name="password"
+                        value={userInfo.password}
+                        onChange={handleInputChange}
                     />
-                    {!visible ? (
-                        <FaEye
-                            onClick={toggleVisible}
-                            className="absolute right-10 bottom-[130px] opacity-[65%]"
-                        />
-                    ) : (
-                        <FaEyeSlash
-                            onClick={toggleVisible}
-                            className="absolute right-10 bottom-[130px] opacity-[65%]"
-                        />
-                    )}
                 </div>
-                <button
+
+                <Button
                     type="button"
-                    className="bg-black text-white px-2 py-1 mx-auto block w-36 transition-colors hover:bg-violet-400 hover:text-black duration-500"
+                    onClick={handleButtonClick}
+                    className="bg-black text-white px-1 py-2 mx-auto w-3/4 transition-colors hover:bg-violet-400 hover:text-black duration-500 flex items-center content-center mt-2"
                 >
-                    Sign Up
-                </button>
+                    <span className="text-[20px]">Log In</span>
+                    <FaArrowRightToBracket className="text-[22px]" />
+                </Button>
             </form>
             <button className="cursor-pointer text-blue-600" onClick={setState}>
                 I already have an account
